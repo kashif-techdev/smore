d692f0e5de2b7ad26fd5a2dd870a652683013d30
kashif <kashif.techdev@gmail.com>
2026-08-01 01:46:16 +0500

    Delete unnecessary file

--- scripts/strip_cursor_coauthor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Git msg-filter helper: remove Cursor co-author trailers from commit messages."""
-
-import sys
-
-for line in sys.stdin:
-    if "Co-authored-by: Cursor" not in line:
-        sys.stdout.write(line)
